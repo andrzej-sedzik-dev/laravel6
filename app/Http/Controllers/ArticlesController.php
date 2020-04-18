bd5fffd2c3e1dcbf183e94c127f760028df42c0b
Andrea <andrzej.sedzik.dev@gmail.com>
2020-04-18 22:17:07 +0200

    The Seven Restful Controller Action

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -8,20 +8,49 @@ use function GuzzleHttp\Promise\all;
 
 class ArticlesController extends Controller
 {
-    public function show($id)
-    {
-        $article = Article::find($id);
-        return view('articles.show', ['article' => $article]);
-    }
+
 
     public function index()
     {
+        // show a list of resource.
         $articles = Article::all();
         return view('articles.index', ['articles' => $articles]);
     }
 
+    public function show($id)
+    {
+        // show a single resource.
+        $article = Article::find($id);
+        return view('articles.show', ['article' => $article]);
+    }
+
+    public  function create()
+    {
+        //Shows view to create resource.
+
+    }
+
+    public  function store()
+    {
+        // Persit the new resource (przechwytauje dane z formularzea create)
+    }
 
 
 
+
+    public  function edit()
+    {
+        // Show view to edit an existing resource
+    }
+
+    public  function update()
+    {
+        // Persist the edited resource.
+    }
+
+    public  function destroy ()
+    {
+        // Delete the resource
+    }
 
 }
