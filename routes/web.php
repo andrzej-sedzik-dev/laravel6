a37bdf416711999eec765a0ea53da71b463edc79
Andrea <andrzej.sedzik.dev@gmail.com>
2020-04-18 14:46:07 +0200

    Layout Pages

--- routes/web.php
+++ routes/web.php
@@ -78,7 +78,16 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 
-Route::get('/posts/{post}',  'PostController@show');
+//Route::get('/posts/{post}',  'PostController@show');
+Route::get('/',  function (){
+    return view('welcome') ;
+});
+
+Route::get('/contact',  function (){
+    return view('contact') ;
+});
+
+
 
 
 
