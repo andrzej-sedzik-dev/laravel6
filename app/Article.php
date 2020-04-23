64a1612d71bbf4b60ed3941a6a480027299acb5f
Andrea <andrzej.sedzik.dev@gmail.com>
2020-04-23 20:27:14 +0200

    Undestending Foreign Keys Database Factories

--- app/Article.php
+++ app/Article.php
@@ -8,4 +8,10 @@ class Article extends Model
 {
     //protected $fillable = ['title', 'excerpt', 'body'] ;
     protected $guarded = []; // wsztkie pola są dostępne
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
+
