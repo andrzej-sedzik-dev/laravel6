7bacdbf4adb9de1420206950d68c1287e8ce79c8
Andrea <andrzej.sedzik.dev@gmail.com>
2020-04-26 22:28:48 +0200

    Fasads

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Example;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class PagesController extends Controller
 {
-    public function home()
+/*    public function home()
     {
         ddd(resolve('App\Example'), resolve('App\Example'));
+    }*/
+
+    public function home()
+    {
+        return View::make('welcome');
     }
+
+
+
+
 }
